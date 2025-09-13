ff9d9d978025215d1989d3416f82302828836c03
Vitzkrieg <dustin@vitzkrieg.net>
2025-09-13 13:49:24 -0400

    compile with calendar.js

---
+++ js/admin.js
@@ -0,0 +1,14 @@
+/*!
+  * Understrap v1.2.4 (https://understrap.com)
+  * Copyright 2013-2025 The Understrap Authors (https://github.com/understrap/understrap/graphs/contributors)
+  * Licensed under GPL-3.0 (https://www.gnu.org/licenses/gpl-3.0.html)
+  */
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+})((function () { 'use strict';
+
+
+
+}));
+//# sourceMappingURL=admin.js.map
